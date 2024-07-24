c296464c2bbe42d3576b04632e940ac4f94da812
DrewJSquared <127908019+DrewJSquared@users.noreply.github.com>
2024-07-24 16:26:57 -0500

    redo duplicate logs handling mechanism

--- Logger.mjs
+++ Logger.mjs
@@ -10,8 +10,12 @@ import eventHub from './EventHub.mjs';
 
 // variables & options
 const LAPTOP_MODE = (process.platform == 'darwin');
-const MAX_TIME_SINCE_DUPLICATE_LOG_SHOWN = 5000; // max interval in ms to ensure duplicate logs are still shown at some point
 const DEV_MODE = false; // if set to true, logs will still show even on raspberry pi units
+
+const MAX_TIME_SINCE_DUPLICATE_LOG_SHOWN = 5000; // max interval in ms to ensure duplicate logs are still shown at some point
+const CLEANUP_INTERVAL = 10000; // interval to clean up old log entries in ms
+const SUPER_HIGH_DUPLICATE_COUNT = 250; // # of duplicates before rate of showing duplicates scales back
+const SUPER_HIGH_DUPLICATE_RATE = 60000; // rate at which super high duplicate counts are shown
 
 
 // create the class
@@ -30,6 +34,15 @@ class Logger {
 
 	    // hold on to the timestamp since the last duplicate log was shown
     	this.duplicateLogTimestamp = null;
+
+    	// store recent log entries and their counts in a map
+    	this.logHistory = new Map();
+
+		// timer to clean up old log entries
+		setInterval(() => {
+            // clean up old logs function
+            this.cleanupOldLogs();
+        }, CLEANUP_INTERVAL);
 	}
 
 
@@ -38,14 +51,125 @@ class Logger {
 		// clean up the type parameter (trim & lowercase)
 		type = type.trim().toLowerCase();
 
-		// create a logEntry object for this log
+		// generate a duplicateLogEntryKey to serve as the key in the map for this log
+		const duplicateLogEntryKey = `${type} | ${message}`;
+
+		// check if this log entry key exists in the logHistory map
+		if (this.logHistory.has(duplicateLogEntryKey)) {
+			// if it does exist, then we have a duplicate log
+
+			// grab the duplicateLogEntry object out of the map
+			const duplicateLogEntry = this.logHistory.get(duplicateLogEntryKey);
+
+			// update the duplicate log count
+			duplicateLogEntry.count++;
+
+			// update the last timestamp, which was the last time a new duplicate was added
+			duplicateLogEntry.lastTimestamp = Date.now();
+
+			// calculate the max time for the interval depending on the # of duplicate logs
+			let maxTimeAdjustedForCount = MAX_TIME_SINCE_DUPLICATE_LOG_SHOWN;
+
+			// once duplicate log count is super high, we get the gist, so scale back the rate at which duplicates are shown
+			if (duplicateLogEntry.count > SUPER_HIGH_DUPLICATE_COUNT) {
+				maxTimeAdjustedForCount = SUPER_HIGH_DUPLICATE_RATE; // scale back the rate
+			}
+
+			// if it's been more than MAX_TIME seconds since this duplicate log has been shown
+			if (Date.now() - duplicateLogEntry.lastShown > maxTimeAdjustedForCount) {
+				// create a log entry, print it, and emit the event
+				let duplicateMessage = `${duplicateLogEntry.count} DUPLICATES | ${duplicateLogEntry.message}`;
+				this.createPrintAndEmitLogEntry(type, duplicateMessage);
+
+				// update the lastShown parameter on the duplicateLogEntry to the current timestamp
+				// lastShown is the last time this duplicate log was actually printed to console
+				duplicateLogEntry.lastShown = Date.now();
+			}
+		} else {
+			// if it doesn't exist in the logHistory map, we need to add it
+
+			// create a duplicateLogEntry object to store this log entry, and data regarding any further duplicates
+			const duplicateLogEntry = {
+				timestamp: new Date().toISOString(),
+				module: this.moduleName,
+				type: type,
+				message: message,
+				count: 1,
+				lastTimestamp: Date.now(),
+				lastShown: Date.now(),
+			}
+
+			// save this duplicateLogEntry under the key from earlier
+			this.logHistory.set(duplicateLogEntryKey, duplicateLogEntry);
+
+			// create a log entry, print it, and emit the event
+			this.createPrintAndEmitLogEntry(type, message);
+		}
+	}
+
+
+	// cleanupOldLogs - cleans up old logs in the logHistory map
+	// print any that haven't been shown (lastShown) in 10 seconds or more
+	// remove any that haven't been updated (lastTimestamp) in 10 seconds or more
+	cleanupOldLogs() {
+		// iterate over each duplicateLogEntry in the logHistory map
+		this.logHistory.forEach((duplicateLogEntry, key) => {
+			// calculate the difference in ms since the last time this duplicate was shown or a new one was added
+			let lastShownDiff = Date.now() - duplicateLogEntry.lastShown;
+			let lastTimestampDiff = Date.now() - duplicateLogEntry.lastTimestamp;
+
+			// calculate if the number of logs in this duplicate is > than super high count
+			let notALotOfLogs = duplicateLogEntry.count < SUPER_HIGH_DUPLICATE_COUNT;
+
+			// if this log both hasn't been shown and hasn't been updated in CLEANUP_INTERVAL
+			if (lastShownDiff >= CLEANUP_INTERVAL && lastTimestampDiff >= CLEANUP_INTERVAL) {
+				// then it can safely be deleted
+				this.logHistory.delete(key);
+			} else if (lastShownDiff >= CLEANUP_INTERVAL && notALotOfLogs) {
+				// otherwise, if this log hasn't been shown in a while AND it's not super high count, then show it
+
+				// create a log entry, print it, and emit the event
+				let duplicateMessage = `${duplicateLogEntry.count} DUPLICATES | ${duplicateLogEntry.message}`;
+				this.createPrintAndEmitLogEntry(duplicateLogEntry.type, duplicateMessage);
+			}
+		});
+	}
+
+
+	// createPrintAndEmitLogEntry - create a log entry object, print it to console, and emit a log event
+	createPrintAndEmitLogEntry(type, message) {
+		// now create a standard logEntry object for this log.
+		// this logEntry object is what will be printed and sent to the network module
 		const logEntry = {
 			timestamp: new Date().toISOString(),
 			module: this.moduleName,
-			type,
-			message,
+			type: type,
+			message: message,
 		};
 
+		// now log the new message in console
+		this.printLogToConsole(logEntry);
+
+		// emit a log event to send this log to the network module
+		this.emitLogEvent(logEntry);
+	}
+
+
+
+
+
+
+
+
+		// console.log('CURRENT MAP: ', this.logHistory);
+
+
+
+
+		// TEMP
+
+
+/*
 		// if this log is the exact same as the last one, simply increase the duplicate count
 		if (this.lastLogEntry && this.lastLogEntry.message === message && this.lastLogEntry.type === type) {
 			// increment the duplicate counter
@@ -82,20 +206,32 @@ class Logger {
 			// reset the duplicate log count
 			this.duplicateLogCount = 0;
 		}
-	}
+
+*/
 
 
-	// grab any duplicate logs and print them to console
-	printDuplicateLogsToConsole() {
-		// grab the last log
-		var duplicateLogEntry = JSON.parse(JSON.stringify(this.lastLogEntry));
+	
 
-		// change the message on it to note the duplicates
-		duplicateLogEntry.message = `${this.duplicateLogCount + 1} DUPLICATES | ${duplicateLogEntry.message}`;
 
-		// actually print the log to console
-		this.printLogToConsole(duplicateLogEntry);
-	}
+	// // grab any duplicate logs and print them to console
+	// printDuplicateLogsToConsole() {
+	// 	// grab the last log
+	// 	var duplicateLogEntry = JSON.parse(JSON.stringify(this.lastLogEntry));
+
+	// 	// change the message on it to note the duplicates
+	// 	duplicateLogEntry.message = `${this.duplicateLogCount + 1} DUPLICATES | ${duplicateLogEntry.message}`;
+
+	// 	// actually print the log to console
+	// 	this.printLogToConsole(duplicateLogEntry);
+	// }
+
+
+
+
+
+
+
+
 
 
 	// printLogToConsole - actually take a log entry and print it to console
@@ -146,8 +282,11 @@ class Logger {
 					console.warn(`UNKNOWN LOG TYPE "${logEntry.type}" | ${logEntry.module} | ${logEntry.message}`);
 				}
 		}
+	}
 
 
+	// emitLogEvent - emit a log event to eventHub
+	emitLogEvent(logEntry) {
 		// emit 'log' event to the eventHub, which can then be picked up by the network module to send to server
 		eventHub.emit('log', logEntry);
 	}
